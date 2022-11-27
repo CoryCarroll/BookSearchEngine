2e474904b2c8652f5e9045f0a18ff5a4b13f7a60
CorysPC <corystory2@gmail.com>
2022-11-27 08:03:06 -0700

    fixed resolver issues, app now deploying to apollo server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,23 @@
-const { User, Book} = require('../models');
+const { User } = require("../models");
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    // users: async () => {
-    //   return await User.find({});
-    // },
-    // books: async (parent, { _id }) => {
-    //   const params
-    // }
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('not logged in ')
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args)
+        const token = signToken(user)
+        return {token, user}
+    }
   }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,44 @@
+
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Mutation {
-    login: ID
-    addUser: String
-    saveBook: ID
-    removeBook: ID
-  }
-
-  type Query {
-    me: 
-  }
-
-  type User {
-    -id:
-    username: String
-    email:
+type User {
+    _id: ID!
+    username: String!
+    email: String
     bookCount: Int
     savedBooks: [Book]
-  }
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+input BookInput{
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+`
 
-  type Book {
-    bookId:
-    authors:
-    description:
-    title:
-    image:
-    link:
-  }
-
-  type Auth {
-    token:
-    user:
-  }
-`;
 
 module.exports = typeDefs;
